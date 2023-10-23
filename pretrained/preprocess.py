ac3051f04607d30d08728ef70b33d220c3a755cc
Rafael Toledo <rafaeltol@gmail.com>
2023-10-23 21:36:24 +0000

    add code for dinov2

--- pretrained/preprocess.py
+++ pretrained/preprocess.py
@@ -1,11 +1,11 @@
 import torch
 import wget
 
-def preprocess(model, name='dino', embed_dim=384):
+def preprocess(model, name='dino', embed_dim=384, patch_size=16):
     new_model = {}
     for k in model.keys():
         if 'patch_embed.proj.weight' in k:
-            x = torch.zeros(embed_dim, 4, 16, 16)
+            x = torch.zeros(embed_dim, 4, patch_size, patch_size)
             x[:, :3] = model[k]
             new_model['backbone.'+k] = x
         else:
@@ -16,12 +16,18 @@ def preprocess(model, name='dino', embed_dim=384):
         size='b'
     torch.save(new_model, name+'_vit_'+ size + '_fna.pth')
 
+
 if __name__ == "__main__":
 
-    wget.download('https://dl.fbaipublicfiles.com/dino/dino_deitsmall16_pretrain/dino_deitsmall16_pretrain.pth')
-    wget.download('https://dl.fbaipublicfiles.com/mae/pretrain/mae_pretrain_vit_base.pth')
+    # wget.download('https://dl.fbaipublicfiles.com/dino/dino_deitsmall16_pretrain/dino_deitsmall16_pretrain.pth')
+    # dino_model = torch.load('dino_deitsmall16_pretrain.pth')
+    # preprocess(dino_model, 'dino', 384)
 
-    dino_model = torch.load('dino_deitsmall16_pretrain.pth')
-    mae_model = torch.load('mae_pretrain_vit_base.pth')['model']
-    preprocess(dino_model, 'dino', 384)
-    preprocess(mae_model, 'mae', 768)
+    # wget.download('https://dl.fbaipublicfiles.com/mae/pretrain/mae_pretrain_vit_base.pth')
+    # mae_model = torch.load('mae_pretrain_vit_base.pth')['model']
+    # preprocess(mae_model, 'mae', 768)
+
+    # wget.download('https://dl.fbaipublicfiles.com/dinov2/dinov2_vitb14/dinov2_vitb14_pretrain.pth')
+    dinov2_model = torch.load('dinov2_vitb14_pretrain.pth')
+    import pdb; pdb.set_trace()
+    preprocess(dinov2_model, 'dinov2', 768, patch_size=14)
